cbf37f3db98bc399f086d99698031427ba11e57f
Linda Mercado <lindacmercado@gmail.com>
2016-02-26 19:59:56 -0700

    update

--- components/home/homeCtrl.js
+++ components/home/homeCtrl.js
@@ -1,8 +1,7 @@
 angular.module('quizApp')
 
-.controller('homeCtrl', ['$scope', 'quizList', 'quizService', function($scope, quizList, quizService){
-    //console.log(pastQuizList);
+.controller('homeCtrl', function($scope, quizList, pastQuizList) {
+    console.log(pastQuizList)
     $scope.quizzes = quizList;
-
-
-}]);
+    $scope.pastQuizzes = pastQuizList;
+});
--- components/quiz/quizCtrl.js
+++ components/quiz/quizCtrl.js
@@ -1,6 +1,41 @@
 angular.module('quizApp')
 
 .controller('quizCtrl', function($scope, $stateParams, quizService, questions){
-    //$scope.questions = questions;
+
+    $scope.questions = questions;
+    $scope.answers = {};
+    $scope.currentQuestion = $scope.questions[0];
+    $scope.results = {};
+
+    $scope.saveAnswer = function(answer) {
+      $scope.answers[$scope.currentQuestion.id] = answer;
+      $scope.nextQuestion();
+
+      if($scope.results.done) {
+        $scope.checkMyAnswers();
+      }
+    };
+
+    $scope.setCurrentQuestion = function(question) {
+      $scope.currentQuestion = question;
+    }
+
+    $scope.update = function(choice) {
+      $scope.selected = choice;
+    }
+
+    $scope.nextQuestion = function() {
+      var idx = $scope.questions.indexOf($scope.currentQuestion);
+      if($scope.questions[idx + 1]) {
+        $scope.currentQuestion = $scope.questions[idx + 1];
+      } else {
+        return;
+      }
+    }
+
+    $scope.reset = function() {
+      $scope.answers = {};
+      $scope.currentQuestion = $scope.questions[0];
+    }
 
 });
